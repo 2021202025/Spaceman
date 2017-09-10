e6b33708559f3b811a791d526892a9b62bab8358
Manu Gupta <gmanu21097@gmail.com>
2017-09-10 22:52:26 +0530

    Missile Script and working

---
+++ Spaceman Creig/Assets/Scripts/projectileController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileController : MonoBehaviour {
+
+	public float rocketSpeed;
+
+	Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Awake () {
+		
+		myRigidBody = GetComponent<Rigidbody2D> ();
+
+		if(transform.localRotation.z > 0)
+			myRigidBody.AddForce (new Vector2 (-1, 0) * rocketSpeed, ForceMode2D.Impulse);
+		else
+			myRigidBody.AddForce (new Vector2 (1, 0) * rocketSpeed, ForceMode2D.Impulse);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
